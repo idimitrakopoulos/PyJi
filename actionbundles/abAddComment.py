062306e81f19eb3cb83fd49bf07595172d067f21
Iason Dimitrakopoulos <idimitrakopoulos@gmail.com>
2015-09-24 23:11:50 +0300

    Create abAddComment.py

---
+++ actionbundles/abAddComment.py
@@ -0,0 +1,31 @@
+from util.Toolkit import log, jiraAuth, scriptGlobals
+from actionbundles.ActionBundle import ActionBundle
+
+
+class abAddComment(ActionBundle):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, parser):
+        '''
+        Constructor
+        '''
+        self.parser = parser
+
+        log.info(self.__class__.__name__ + " initialized")
+
+        try:
+            jira = jiraAuth(scriptGlobals.jiraURL, scriptGlobals.jiraUsername, scriptGlobals.jiraPassword)
+
+            # Get an issue.
+            issue = jira.issue(parser.options.key)
+
+            # Add a comment to the issue
+            jira.add_comment(issue, parser.options.comment)
+
+            log.info("Issue Key: " + parser.options.key)
+            log.info("Comment  : " + parser.options.comment)
+
+        except:
+            raise
